f925a0afb2aaf8df33486dee8cb107de0b2e8bde
Your Name <sanglibazaar.com@gmail.com>
2025-06-25 17:24:53 +0530

    Recommitted files with proper line endings

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,20 +19,7 @@
     </div>
 @endsection@extends('layouts.app')
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-                    <div class="card-body">
-                        <p>Welcome, {{ Auth::user()->name }}!</p>
-                        <a href="{{ route('logout') }}" class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </d@extends('layouts.app')
+
 
 @section('content')
     <div class="container">
